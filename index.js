1c712299b28e584f5aebd158af4625590b8615f7
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-03-15 16:24:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ console.log(multiply);
 
 // a variable random that generates a random integer greater than 0
 let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
 
 // a variable mod that calculates the remainder of two numbers
 let num3 = 11;
 let num4 = 7;
 
 let mod = (num3 % num4);
+console.log(mod);
 
 // a variable max that finds the highest number in a set
 let max = Math.max(4, 6, 13, 2, 20, 9);
+console.log(max);
